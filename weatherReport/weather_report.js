d2f13c72547d3a1d14901be696030ea47218ed47
Mardino229 <maxdassi01@gmail.com>
2025-01-20 08:15:52 -0500

    weather app

---
+++ weatherReport/weather_report.js
@@ -0,0 +1,22 @@
+
+function showweatherDetails(event) {
+    event.preventDefault();
+    const city = document.getElementById('city').value;
+    const apiKey = '966be8a39d5e5181d76a526fb4ff9e03'; // Remplacez 'YOUR_API_KEY' par votre véritable clé API
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+     fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+          const weatherInfo = document.getElementById('weatherInfo');
+          weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+                                  <p>Temperature: ${data.main.temp} &#8451;</p>
+                                  <p>Weather: ${data.weather[0].description}</p>`;
+        })
+        .catch(error => {
+            console.error('Erreur lors de la récupération des données météo :', error);
+            const weatherInfo = document.getElementById('weatherInfo');
+            weatherInfo.innerHTML = `<p>Échec de la récupération des données météo. Veuillez réessayer.</p>`;
+          });
+}
+
+ document.getElementById('weatherForm').addEventListener('submit',showweatherDetails );
